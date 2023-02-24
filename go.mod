719cfb2b74e7cfcad02e01cb0566db793adf6a66
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-02-23 18:44:30 -0800

    Update ffs module, remove Size method.

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/creachadair/nutstore
 go 1.20
 
 require (
-	github.com/creachadair/ffs v0.0.0-20230205181227-d55aafbb6c81
+	github.com/creachadair/ffs v0.0.0-20230223181110-ea7f85e506d3
 	github.com/xujiajun/nutsdb v0.11.1
 )
 
 require (
 	github.com/bwmarrin/snowflake v0.3.0 // indirect
-	github.com/creachadair/mds v0.0.0-20230121043849-c7d6542927f1 // indirect
+	github.com/creachadair/mds v0.0.0-20230211201837-5ec6beec55ce // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/xujiajun/mmap-go v1.0.1 // indirect
--- nutstore.go
+++ nutstore.go
@@ -102,23 +102,6 @@ func (s Store) Put(_ context.Context, opts blob.PutOptions) error {
 	})
 }
 
-// Size implements part of blob.Store.
-func (s Store) Size(_ context.Context, key string) (int64, error) {
-	var size int64
-	if err := s.db.View(func(tx *nutsdb.Tx) error {
-		e, err := tx.Get(s.bucket, []byte(key))
-		if err == nil {
-			size = int64(len(e.Value))
-		}
-		return err
-	}); isNutFound(err) {
-		return 0, blob.KeyNotFound(key)
-	} else if err != nil {
-		return 0, err
-	}
-	return size, nil
-}
-
 // Delete implements part of blob.Store.
 func (s Store) Delete(ctx context.Context, key string) error {
 	return s.db.Update(func(tx *nutsdb.Tx) error {
